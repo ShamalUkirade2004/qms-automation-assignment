3ea029c8489cb3416911e271e3bac4cf57a64b9f
ShamalUkirade2004 <ukiradeshamal26@gmail.com>
2026-05-09 08:24:18 +0530

    Create DriverManager.java

---
+++ src/test/resources/src/test/resources/src/test/java/com/qms/utils/src/test/java/com/qms/utils/DriverManager.java
@@ -0,0 +1,74 @@
+package com.qms.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+import java.time.Duration;
+
+/**
+ * DriverManager - Manages WebDriver lifecycle using ThreadLocal for parallel execution.
+ */
+public class DriverManager {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void initDriver() {
+        String browser = ConfigReader.getBrowser().toLowerCase();
+        boolean headless = ConfigReader.isHeadless();
+        WebDriver webDriver;
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (headless) firefoxOptions.addArguments("--headless");
+                webDriver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if (headless) edgeOptions.addArguments("--headless");
+                webDriver = new EdgeDriver(edgeOptions);
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--start-maximized");
+                webDriver = new ChromeDriver(chromeOptions);
+                break;
+        }
+
+        webDriver.manage().timeouts().implicitlyWait(
+                Duration.ofSeconds(ConfigReader.getImplicitWait()));
+        webDriver.manage().window().maximize();
+
+        driver.set(webDriver);
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
